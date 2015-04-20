6df08cb94b2224fe14d908e063d664575db29a02
Oscar Lopez <oscar.lopez@ciencias.unam.mx>
2015-04-20 14:52:59 -0500

    actualizado hasta el notebook 8

--- Intervalos.jl
+++ Intervalos.jl
@@ -90,5 +90,66 @@ function Intervalo(x::Real)
     Intervalo(x,x)
 end
 
+function ^(x::Intervalo, n::Integer)
+    if mod(n,2)!=0
+        r=Intervalo(x.a^n, x.b^n)
+    else
+        if x.a>=0
+            r=Intervalo(x.a^n, x.b^n)
+        elseif x.b<0
+            r=Intervalo(x.b^n, x.a^n)
+        else
+            r=Intervalo(0, max(x.a^n, x.b^n))
+        end
+    end
+end
+
+function ^(x::Intervalo, a::FloatingPoint)
+    r=Intervalo(0,0)
+    if mod(a,1)==0
+        n=int(a)
+        r=x^n
+    else
+        if x.a<0 || x.b<0
+            throw(DomainError())
+        else
+            r=Intervalo(x.a^a, x.b^a)#ya que x^a es monótonamente creciente
+        end
+    end
+    r
+end
+         
+function +(x::Intervalo,y::Real)
+    x+Intervalo(y)
+end
+
+function +(x::Real,y::Intervalo)
+    Intervalo(x)+y
+end
+
+function -(x::Intervalo,y::Real)
+    x-Intervalo(y)
+end
+
+function -(x::Real,y::Intervalo)
+    Intervalo(x)-y
+end
+
+function *(x::Intervalo,y::Real)
+    x*Intervalo(y)
+end
+
+function *(x::Real,y::Intervalo)
+    Intervalo(x)*y
+end
+
+function monot(x::Intervalo, f::Function)
+    if f(x.a)<f(x.b)
+        Intervalo(f(x.a),f(x.b))
+    else
+        Intervalo(f(x.b),f(x.a))
+    end
+end   
+
 end
 
